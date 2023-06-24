7a41a94885d8a3b05c610dd5b0035b41408732ca
windowslucker <schneider@ashomenetwork.de>
2023-06-24 14:10:13 +0200

    WA: receiving messages and voice messages as pure data, conversion still missing

---
+++ message.js
@@ -0,0 +1,11 @@
+﻿// class message {
+//     @param {message} wholeMessage 
+//     constructor(wholeMessage) {
+//         const stringContactName = (await wholeMessage.getContact()).pushname;
+//         const messageBody = wholeMessage.body;
+//     }
+//
+//     print() {
+//         console.log(this.name + " : " + this.message);
+//     }
+// }
--- whatsapp_connection.js
+++ whatsapp_connection.js
@@ -19,8 +19,36 @@ client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.on('message', message => {
-	console.log(message.body);
+client.on('message', async message => {
+    const stringContactName = (await message.getContact()).pushname;
+    
+    if(message.hasMedia) {
+        console.log("Received media. Starting download.");
+        const media = await message.downloadMedia();
+        if(media == undefined )
+        {
+            Console.log("ERROR: media is somehow corrupted/deleted.");
+        }
+        else
+        {
+            console.log("MediaInformation: \n   filename: " + media.fileName +
+                "\ntype: " + media.mimetype +
+                "\nfilesize: " + media.filesize +
+                "\ndata: " + media.data);
+        }
+        
+        // do something with the media data here
+    }
+    else
+    {
+        const messageBody = message.body;
+        console.log (stringContactName + ": " + messageBody);
+    }
+    
+    
+
+    
+
 });
  
 client.initialize();
