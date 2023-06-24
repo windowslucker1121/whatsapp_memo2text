8e362fbae916a09dca5fd0369fe6055acb98ee05
windowslucker <schneider@ashomenetwork.de>
2023-06-24 09:36:48 +0200

    session saving, auth data is saved in ".wwebjs_auth" directory

---
+++ whatsapp_connection.js
@@ -0,0 +1,27 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const client = new Client(
+    {
+        authStrategy : new LocalAuth(
+            {
+                clientId: "self"
+            }
+        )
+    }
+);
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+	console.log(message.body);
+});
+ 
+client.initialize();
+ 
