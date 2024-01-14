d41e5c712b7620dcbffaa55a218a3cb15afd3b14
msc94 <msc94@users.noreply.github.com>
2024-01-14 23:57:04 +0100

    Some formatting changes

--- ai/main.py
+++ ai/main.py
@@ -34,10 +34,11 @@ async def call_whisper(filename: str, data: bytes, mimetype: str):
         headers = {"Authorization": f"Bearer {OPENAI_API_KEY}"}
 
         async with session.post(
-                url=WHISPER_URL,
-                headers=headers,
-                data=form_data) as response:
-            logging.debug(f"Whisper response: {response.status}, {await response.text()}")
+            url=WHISPER_URL, headers=headers, data=form_data
+        ) as response:
+            logging.debug(
+                f"Whisper response: {response.status}, {await response.text()}"
+            )
             response.raise_for_status()
             return await response.json()
 
@@ -57,14 +58,16 @@ async def call_chatgpt(system_prompt: str, prompt: str):
                 {
                     "role": "user",
                     "content": prompt,
-                }
-            ]}
+                },
+            ],
+        }
 
         async with session.post(
-                url=CHATGPT_URL,
-                headers=headers,
-                json=json) as response:
-            logging.debug(f"ChatGPT response: {response.status}, {await response.text()}")
+            url=CHATGPT_URL, headers=headers, json=json
+        ) as response:
+            logging.debug(
+                f"ChatGPT response: {response.status}, {await response.text()}"
+            )
             response.raise_for_status()
             return await response.json()
 
@@ -84,13 +87,15 @@ async def transcribe():
     mime_type = file.mimetype
 
     logging.debug(
-        f"Received file {filename} with type {mime_type} and {len(data)} bytes of data")
+        f"Received file {filename} with type {mime_type} and {len(data)} bytes of data"
+    )
 
     if mime_type != "audio/mpeg":
         data, filename = transcode(data, mime_type, "audio/mpeg")
         mime_type = "audio/mpeg"
         logging.debug(
-            f"Transcoded data to {filename} ({mime_type}) with {len(data)} bytes of data")
+            f"Transcoded data to {filename} ({mime_type}) with {len(data)} bytes of data"
+        )
 
     result = await call_whisper(filename, data, mime_type)
     transcription = result["text"]
@@ -110,6 +115,4 @@ async def summarize():
     chatgpt_result = await call_chatgpt(SUMMARIZE_SYSTEM_PROMPT, text)
     summary = chatgpt_result["choices"][0]["message"]["content"]
 
-    return {
-        "summary": summary
-    }
+    return {"summary": summary}
